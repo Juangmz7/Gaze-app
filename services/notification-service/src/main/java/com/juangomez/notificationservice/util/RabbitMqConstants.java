544b37063b73ca49839367cf5534de83b551894a
Juangmz7 <juanjuangmo@gmail.com>
2025-12-21 18:31:50 +0100

    (notification)Implemented rabbitmq constants

---
+++ services/notification-service/src/main/java/com/juangomez/notificationservice/util/RabbitMqConstants.java
@@ -0,0 +1,43 @@
+package com.juangomez.notificationservice.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import lombok.Getter;
+
+@Component
+@Getter
+public class RabbitMqConstants {
+
+    // --- Queues ---
+    @Value("${rabbitmq.queue.tag.created}")
+    private String queueTagCreated;
+
+    @Value("${rabbitmq.queue.post.liked}")
+    private String queuePostLiked;
+
+    @Value("${rabbitmq.queue.comment.created}")
+    private String queueCommentCreated;
+
+    // --- Exchanges ---
+    @Value("${rabbitmq.exchange.post.events}")
+    private String exchangePostEvents;
+
+    @Value("${rabbitmq.exchange.user.commands}")
+    private String exchangeUserCommands;
+
+    @Value("${rabbitmq.exchange.user.events}")
+    private String exchangeUserEvents;
+
+    // --- Routing Keys ---
+    @Value("${rabbitmq.rk.tag.created}")
+    private String rkTagCreated;
+
+    @Value("${rabbitmq.rk.post.liked}")
+    private String rkPostLiked;
+
+    @Value("${rabbitmq.rk.comment.created}")
+    private String rkCommentCreated;
+
+    @Value("${rabbitmq.rk.notification.sent}")
+    private String rkNotificationSent;
+}
