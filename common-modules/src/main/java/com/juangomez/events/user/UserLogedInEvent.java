1f14c480876f638cbc8ec16c2a7582230649d3d4
Juangmz7 <juanjuangmo@gmail.com>
2025-12-23 15:20:33 +0100

    (common)Fixed record name

--- common-modules/src/main/java/com/juangomez/events/user/UserLogedInEvent.java
+++ common-modules/src/main/java/com/juangomez/events/user/UserLogedInEvent.java
@@ -6,7 +6,7 @@ import java.time.Instant;
 import java.util.UUID;
 
 
-public record UseLogedInEvent (
+public record UserLogedInEvent(
         UUID messageId,
         Instant occurredAt
 ) implements DomainMessage {}
