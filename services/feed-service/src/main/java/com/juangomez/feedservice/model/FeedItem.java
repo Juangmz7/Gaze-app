1d8ec366d465a9e364fe565b2579ec6e0c39214b
Juangmz7 <juanjuangmo@gmail.com>
2025-12-23 16:09:24 +0100

    Create FeedItem.java

---
+++ services/feed-service/src/main/java/com/juangomez/feedservice/model/FeedItem.java
@@ -0,0 +1,36 @@
+package com.juangomez.feedservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+// Entity used for reading posts info faster
+@Entity
+@NoArgsConstructor
+@Getter
+public class FeedItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private UUID userId;
+
+    private UUID postId;
+
+    private String postBody;
+
+    private long likeCount;
+
+    private long commentCount;
+
+    private String firstComment;
+
+}
