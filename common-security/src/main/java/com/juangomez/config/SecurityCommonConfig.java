97549b9a581949cf4697954cb7cdf656123262f3
Juangmz7 <juanjuangmo@gmail.com>
2025-12-28 15:24:56 +0100

    Implemented common-security module

---
+++ common-security/src/main/java/com/juangomez/config/SecurityCommonConfig.java
@@ -0,0 +1,47 @@
+package com.juangomez.config;
+
+import com.juangomez.filter.GatewayTrustFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity // Enables @PreAuthorize("hasRole('ADMIN')")
+public class SecurityCommonConfig {
+
+    // Define public endpoints (Swagger + Actuator)
+    private static final String[] AUTH_WHITELIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/actuator/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                // Disable CSRF as we are using a stateless REST API
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // Set session management to stateless (no JSESSIONID)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // Define permission rules
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AUTH_WHITELIST).permitAll() // Allow Swagger & Actuator
+                        .anyRequest().authenticated() // Everything else requires X-User-Id header
+                )
+
+                // Add our custom GatewayTrustFilter before the standard UsernamePasswordAuthenticationFilter
+                .addFilterBefore(new GatewayTrustFilter(), UsernamePasswordAuthenticationFilter.class)
+
+                .build();
+    }
+}
