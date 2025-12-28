c9532e8567dd6973c466c0dc9ff4ce07eaccb87d
Juangmz7 <juanjuangmo@gmail.com>
2025-12-28 16:25:07 +0100

    (common)Added automatic imports file

--- common-security/src/main/java/com/juangomez/config/SecurityCommonConfig.java
+++ common-security/src/main/java/com/juangomez/config/SecurityCommonConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -43,5 +45,10 @@ public class SecurityCommonConfig {
                 .addFilterBefore(new GatewayTrustFilter(), UsernamePasswordAuthenticationFilter.class)
 
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
     }
 }
