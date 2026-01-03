12e11625d3080289ea6ad1be3b8523fd3ed7b9e7
Juangmz7 <juanjuangmo@gmail.com>
2026-01-03 20:32:10 +0100

    Configured services for using eureka client

--- services/social-service/src/main/java/com/juangomez/socialservice/SocialServiceApplication.java
+++ services/social-service/src/main/java/com/juangomez/socialservice/SocialServiceApplication.java
@@ -2,8 +2,10 @@ package com.juangomez.socialservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SocialServiceApplication {
 
     public static void main(String[] args) {
