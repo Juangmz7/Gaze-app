1e4f01d5ab696b0a585ee0c7c96fe2cd8ba45f5a
Juangmz7 <juanjuangmo@gmail.com>
2026-01-21 19:58:39 +0100

    Added containers for testing

--- services/feed-service/src/test/java/com/juangomez/feedservice/FeedServiceApplicationTests.java
+++ services/feed-service/src/test/java/com/juangomez/feedservice/FeedServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.juangomez.feedservice;
 
+import com.juangomez.feedservice.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestContainersConfig.class)
 class FeedServiceApplicationTests {
 
     @Test
--- services/social-service/src/test/java/com/juangomez/socialservice/SocialServiceApplicationTests.java
+++ services/social-service/src/test/java/com/juangomez/socialservice/SocialServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.juangomez.socialservice;
 
+import com.juangomez.socialservice.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestContainersConfig.class)
 class SocialServiceApplicationTests {
 
     @Test
