1c1261e19a82d39688f08eccadbb7be7d3620ee6
Juangmz7 <juanjuangmo@gmail.com>
2026-01-21 14:46:44 +0100

    Deleted eureka from apps

--- services/feed-service/src/main/java/com/juangomez/feedservice/FeedServiceApplication.java
+++ services/feed-service/src/main/java/com/juangomez/feedservice/FeedServiceApplication.java
@@ -2,10 +2,8 @@ package com.juangomez.feedservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class FeedServiceApplication {
 
     public static void main(String[] args) {
