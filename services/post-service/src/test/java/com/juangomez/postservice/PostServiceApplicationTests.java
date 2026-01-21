e3d9f70efc8bbb1f8a054a14dfd4ff9553c9deb2
Juangmz7 <juanjuangmo@gmail.com>
2026-01-21 18:22:09 +0100

    Update PostServiceApplicationTests.java

--- services/post-service/src/test/java/com/juangomez/postservice/PostServiceApplicationTests.java
+++ services/post-service/src/test/java/com/juangomez/postservice/PostServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.juangomez.postservice;
 
+import com.juangomez.postservice.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestContainersConfig.class)
 class PostServiceApplicationTests {
 
     @Test
